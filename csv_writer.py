2e4bde34a4866419184d5febf3ddb0f141277a31
Tippett <David.Tippett@cbn.org>
2019-08-07 10:58:43 -0400

    Potential resolution for special char in csv

--- csv_writer.py
+++ csv_writer.py
@@ -1,5 +1,9 @@
 from datetime import datetime
 
+def contains_special_char(_check_str):
+  special_char = ['\n', ',']
+  return any(st in _check_str for st in special_char)
+
 def export_csv(_data, _path, _filename, _overwrite=False ):
   '''
   Exports Json List as csv
@@ -32,14 +36,20 @@ def export_csv(_data, _path, _filename, _overwrite=False ):
 
     if count == 0:
         for key in header:
-          _filestr += key + ","
+          if contains_special_char(key):
+            _filestr += "\"" + key + "\"" + ","
+          else:
+            _filestr += key + ","
         
         _filestr += "\n"
         count += 1
 
 
     for key in _obj:
-        _filestr += _obj[key] + ","
+        if contains_special_char(_obj[key]):
+          _filestr += "\"" +_obj[key] + "\"" + ","
+        else:
+          _filestr += _obj[key] + ","
 
     _filestr += "\n"
     

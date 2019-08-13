ce2bf5c99805e67c15025fc48527efbff510eb3a
David Tippett <dtippett@Davids-MacBook-Pro-5.local>
2019-08-13 19:46:03 -0400

    Improved code quality csv_writer

--- csv_writer.py
+++ csv_writer.py
@@ -4,54 +4,55 @@ def contains_special_char(_check_str):
   special_char = ['\n', ',']
   return any(st in _check_str for st in special_char)
 
-def export_csv(_data, _path, _filename, _overwrite=False ):
+def process_row(_obj):
+  temp_row = ""
+  for key in _obj:
+    if contains_special_char(_obj[key]):
+      temp_row += "\"" +_obj[key] + "\"" + ","
+    else:
+      temp_row += _obj[key] + ","
+
+  return temp_row += "\n"
+
+def export_csv(_data, parameters):
   '''
   Exports Json List as csv
 
   Parameters: 
     Data (Json list data)
-    Path (Path to put file in)
-    FileName (Name for file)
-    Overwrite (true/false whether you want previous files overwritten)
-      - Defaults to false
+    Args Object = 
+    {
+      path: "C:\\Users\\Data\\",
+      fileName: "MyCSV",
+      overwrite: True/False (will append date and )
+    }
+      Path (Path to put file in)
+      FileName (Name for file)
+      Overwrite (true/false whether you want previous files overwritten)
+        - Defaults to false
   '''
 
+
   if _overwrite:
-    _path = _path + _filename + ".csv"
+    _path = parameters['path'] + parameters['filename'] + ".csv"
   else:
     now = datetime.now
     timestr = datetime.utcnow().strftime('%Y-%m-%d-%f')
     
-    _path = _path + _filename + timestr + ".csv"
+    _path = parameters['path'] + parameters['filename'] + timestr + ".csv"
 
-
-  
   _location = open(_path, 'w+')
   count = 0
   _filestr = ""
 
+  header = _obj.keys()
+  _filestr += process_row(header)
 
   for _obj in _data:
-    header = _obj.keys()
+    _filestr += process_row(_obj)
 
-    if count == 0:
-        for key in header:
-          if contains_special_char(key):
-            _filestr += "\"" + key + "\"" + ","
-          else:
-            _filestr += key + ","
-        
-        _filestr += "\n"
-        count += 1
-
-
-    for key in _obj:
-        if contains_special_char(_obj[key]):
-          _filestr += "\"" +_obj[key] + "\"" + ","
-        else:
-          _filestr += _obj[key] + ","
-
-    _filestr += "\n"
-    
   _location.write(_filestr)
   _location.close()
+
+
+
